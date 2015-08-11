df9cc818703152c19ecb6c6ee90fe8ee06b8567f
Iosnowore <pressinger.gene66@gmail.com>
2015-08-10 21:31:39 -0400

    Changes to AssemblyInfo

--- SWG Expertise Calcualtor/SWG Expertise Calcualtor/Properties/AssemblyInfo.cs
+++ SWG Expertise Calcualtor/SWG Expertise Calcualtor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SWG Expertise Calcualtor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Project SWG Tools")]
+[assembly: AssemblyDescription("Includes an SWG expertise calculator, website and development quick-links, game art and music archives, and more.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SWG Expertise Calcualtor")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Project SWG")]
+[assembly: AssemblyProduct("Project SWG Tools")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0")]
