d2f9bf5826aefe890d2fde1ceb6e389fff5ea44f
Iosnowore <pressinger.gene66@gmail.com>
2015-08-15 15:33:43 -0400

    Removed all unused imports and usings

--- SWG Expertise Calcualtor/SWG Expertise Calcualtor/ExpertiseCalcStart.cs
+++ SWG Expertise Calcualtor/SWG Expertise Calcualtor/ExpertiseCalcStart.cs
@@ -1,12 +1,6 @@
 ﻿using SWG_Expertise_Calcualtor.Controllers.Services;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SWG_Expertise_Calcualtor
--- SWG Expertise Calcualtor/SWG Expertise Calcualtor/StartUpPage.cs
+++ SWG Expertise Calcualtor/SWG Expertise Calcualtor/StartUpPage.cs
@@ -1,12 +1,4 @@
-﻿using SWG_Expertise_Calcualtor.Controllers.Services;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 using System.Diagnostics;
 
